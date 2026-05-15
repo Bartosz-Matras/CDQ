0540ad4466f004e4b8595d388fe944caa7e1c027
Bartosz Matras <bartosz.matras38@gmail.com>
2026-05-15 13:34:24 +0200

    Initialize project structure with Gradle and Spring Boot setup

---
+++ src/main/java/com/bartoszmatras/cdq/CdqApplication.java
@@ -0,0 +1,12 @@
+package com.bartoszmatras.cdq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdqApplication.class, args);
+    }
+}
